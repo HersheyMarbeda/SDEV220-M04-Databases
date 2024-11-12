ab5f8b6a852e7ac1ce2bac826d856cc722bd15b0
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-12 09:10:02 -0500

    Updates

---
+++ zoo.py
@@ -0,0 +1,11 @@
+# Python Program to demonstrate the use of import
+def hours():
+    print('Open 9-5 daily')
+    
+import zoo
+zoo.hours()
+
+import zoo as menagerie
+def hours():
+    print('Open 9-5 daily')
+menagerie.hours()
